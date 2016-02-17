6051eaa8e4e482fb5f636f7e3edb5bc279da6a53
Ryan Dobrinski <ryandobrinski@Macintosh-2.home>
2016-02-17 14:16:10 -0500

    added crud functionality and bootstrap styling

--- config/application.rb
+++ config/application.rb
@@ -1,21 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ExpenseTracker
+module AccountsReactRails
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -31,5 +22,7 @@ module ExpenseTracker
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.react.addons = true
   end
 end
